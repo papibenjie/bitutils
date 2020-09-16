ef8b15a14ba7272105d0630c55938f0cc7efe4d1
marc-antoine.quessy <marc-antoine.quessy@outlook.com>
2020-09-16 11:13:20 -0400

    alklo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return <p>ALLO123</p>;
+  }
+
+}
+
+export default Navbar;
